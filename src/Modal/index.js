3a57772d203a501b8624090c8180eeddef2fbcf0
thejoker10bn <maleesga97@hotmail.com>
2023-06-23 21:56:24 -0500

    portal

---
+++ src/Modal/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { createPortal } from "react-dom";
+
+function Modal({ children }) {
+    return createPortal(
+        <div className="Modal">
+            { children }
+        </div>,
+        document.getElementById('modal')
+    );
+}
+
+
+
+export { Modal };
--- src/TodoContext/index.js
+++ src/TodoContext/index.js
@@ -1,67 +1,66 @@
-import React from "react";
-import { useLocalStorage } from "./useLocalStorage";
+import React from 'react';
+import { useLocalStorage } from './useLocalStorage';
 
- const TodoContext = React.createContext();
+const TodoContext = React.createContext();
 
- function TodoProvider ( { children }) {
+function TodoProvider({ children }) {
+  const {
+    item: todos,
+    saveItem: saveTodos,
+    loading,
+    error,
+  } = useLocalStorage('TODOS_V1', []);
+  const [searchValue, setSearchValue] = React.useState('');
+  const [openModal, setOpenModal] = React.useState(true);
 
-    const {
-        item: todos,
-        saveItem: saveTodos,
-        loading,
-        error,
-      } = useLocalStorage('TODOS_V1', []);
-      const [searchValue, setSearchValue] = React.useState('');
-    
-      const completedTodos = todos.filter(
-        todo => !!todo.completed
-      ).length;
-      const totalTodos = todos.length;
-    
-      const searchedTodos = todos.filter(
-        (todo) => {
-          const todoText = todo.text.toLowerCase();
-          const searchText = searchValue.toLowerCase();
-          return todoText.includes(searchText);
-        }
-      );
-    
-      const completeTodo = (text) => {
-        const newTodos = [...todos];
-        const todoIndex = newTodos.findIndex(
-          (todo) => todo.text === text
-        );
-        newTodos[todoIndex].completed = true;
-        saveTodos(newTodos);
-      };
-    
-      const deleteTodo = (text) => {
-        const newTodos = [...todos];
-        const todoIndex = newTodos.findIndex(
-          (todo) => todo.text === text
-        );
-        newTodos.splice(todoIndex, 1);
-        saveTodos(newTodos);
-      };
+  const completedTodos = todos.filter(
+    todo => !!todo.completed
+  ).length;
+  const totalTodos = todos.length;
 
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    }
+  );
 
-    return (
-        <TodoContext.Provider value={{
-          loading,
-          error,
-          completedTodos,
-          totalTodos,
-          searchValue,
-          setSearchValue,
-          searchedTodos,
-          completeTodo,
-          deleteTodo,
-        }}>
-          { children }
-        </TodoContext.Provider>
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
     );
- }
+    newTodos[todoIndex].completed = true;
+    saveTodos(newTodos);
+  };
 
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+    newTodos.splice(todoIndex, 1);
+    saveTodos(newTodos);
+  };
+  
+  return (
+    <TodoContext.Provider value={{
+      loading,
+      error,
+      completedTodos,
+      totalTodos,
+      searchValue,
+      setSearchValue,
+      searchedTodos,
+      completeTodo,
+      deleteTodo,
+      openModal,
+      setOpenModal,
+    }}>
+      {children}
+    </TodoContext.Provider>
+  );
+}
 
-
- export { TodoContext, TodoProvider };
+export { TodoContext, TodoProvider };
